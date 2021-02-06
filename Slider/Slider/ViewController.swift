2d35a30823b4b2493532c80bc5cc05074a2caf7b
Rajesh Shiyal <imrajesh.coder@gmail.com>
2021-02-06 11:51:29 +0530

    initial commit.

---
+++ Slider/Slider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Vijay on 06/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
